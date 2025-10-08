c4b3167affe9b0c5d190c1d0609ff2d9ae028cec
PhatVo <9a10voletruongphat@gmail.com>
2025-10-08 14:50:39 +0700

    get gemini api key && create api route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+import { GoogleGenAI } from "@google/genai";
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+export const POST = async (req: Request) => {
+  const body = await req.json();
+  console.log(body);
+
+  const model = await ai.models.generateContent({
+    model: "gemini-2.0-flash-001",
+    contents: body.content,
+  });
+
+  return NextResponse.json({ message: ` ${model.text}` });
+};
